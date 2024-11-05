80ee69111f9bc6fcbab58a021c5b7589caca7b43
Inna Penza <penzainna27@gmail.com>
2024-11-06 01:53:00 +0200

    add new func

---
+++ new_1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    double n, s;
+    cout << "Enter n:\n";
+    cin >> n;
+    s = int(n) % 3600;
+    cout << "s = " << s << endl;
+    return 0;
+}
+//тест системи контролю версій
+//коментар 1
